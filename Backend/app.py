198b44b9760461ebb484a8037b5256c108d0adf3
Subhan Asghar <subhanasghar16@gmail.com>
2025-01-27 02:29:47 +0500

    Cors

--- Backend/app.py
+++ Backend/app.py
@@ -2,7 +2,9 @@ from flask import Flask, request, jsonify
 from agent import get_reponse
 from flask_cors import CORS
 app = Flask(__name__)
-CORS(app)
+
+CORS(app, resources={r"/*": {"origins": "*"}})
+
 @app.route('/api', methods=['POST'])
 def greet():
     data = request.json
