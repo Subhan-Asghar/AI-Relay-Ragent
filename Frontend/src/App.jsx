041cc26ee2e24d457edee7a70054fec3466084b9
Subhan Asghar <subhanasghar16@gmail.com>
2025-01-26 14:33:05 +0500

    Nav Bar

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,14 +1,26 @@
-import { useState } from 'react'
-
+import { useState } from 'react';
 
 function App() {
-
-
   return (
     <>
-     <h3 className='bg-green-500 text-black'>Subhan</h3>
+      <nav className="flex items-center justify-between bg-black p-4">
+        {/* Logo */}
+        <div className="flex items-center space-x-2">
+          <img
+            src="/logo-2LzXSqsyE-transformed.png" 
+            alt="logo"
+            className="h-8 w-8 object-cover rounded-full border-2 border-yellow-500"
+          />
+          <div className="text-yellow-500 text-lg font-bold ">Rely</div>
+        </div>
+
+        {/* Button */}
+        <button className="bg-yellow-500 hover:bg-yellow-400 cursor-pointer text-black px-4 py-2 rounded-lg font-medium shadow-lg transition-all">
+          New Chat
+        </button>
+      </nav>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
